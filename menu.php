d9b1b4fa04f7402fe53abeace9b998ff23aee516
JoãoVitor1309 <joaovitorbianchini09@gmail.com>
2022-11-04 08:40:25 -0300

    1.0.2

---
+++ menu.php
@@ -0,0 +1,52 @@
+
+    <nav id="nav" class="navbar navbar-expand-sm navbar-dark bg-success justify-content-center" class="container">
+       
+    <div class="container-fluid">
+        
+    <h4>OSVAC</h4>
+    
+    <ul class="navbar-nav">
+
+        <div class="dropdown">
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+             Cliente
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="cadastrocliente2.php">Cadastrar Cliente</a>
+                <a class="dropdown-item" href="ver_clientes2.php">Listagem de Clientes</a>
+            </div>
+        </div>                
+            
+        <div class="dropdown">
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+              Estoque
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="cadastropecas.php">Cadastrar Peça</a>
+                <a class="dropdown-item" href="verestoque.php">Listagem de Estoque</a>
+            </div>
+        </div> 
+
+        <div class="dropdown">
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+              Funcionários
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="cadastrofunc.php">Cadastrar Funcionário</a>
+                <a class="dropdown-item" href="verfunc.php">Listagem de Funcionários</a>
+            </div>
+        </div> 
+
+        <div class="dropdown">
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+             Serviços
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="cadastroaparelho.php">Cadastrar Serviço</a>
+                <a class="dropdown-item" href="verservicos.php">Listagem de Serviços</a>
+            </div>
+        </div> 
+
+    </ul>
+    
+</nav><br><br><br><br><br><br>
